a989d3109a6d699cb0e6561e7c625ecc69329f2c
Gui-dev <adrianon10@gmail.com>
2020-12-17 03:29:14 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { Home } from './../screens/Home'
+import { Details } from './../screens/Details'
+
+export const Routes: React.FC = () => {
+
+  const { Navigator, Screen } = createStackNavigator()
+
+  return (
+    <NavigationContainer>
+      <Navigator headerMode="none">
+        <Screen 
+          name="Home" 
+          component={ Home }
+        />
+        <Screen 
+          name="Details" 
+          component={ Details }
+        />
+      </Navigator>
+    </NavigationContainer>
+  )
+}
