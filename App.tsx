8cf4894aff719f8736959b61db9c101b418aa7a7
Gui-dev <adrianon10@gmail.com>
2020-12-18 01:40:44 -0300

    feat: add font and routes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import AppLoading from 'expo-app-loading'
+import { Anton_400Regular, useFonts } from '@expo-google-fonts/anton'
 
 import { Routes } from './src/routes'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts([Anton_400Regular])
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="light" backgroundColor="#222" translucent={ true }/>
       <Routes />
     </>
   )
