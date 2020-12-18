d2e7554b9f7512d59d2c4d1bbcebe68ad5db4a7e
Gui-dev <adrianon10@gmail.com>
2020-12-18 01:39:57 -0300

    feat: create page Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-// import { Container } from './styles'
+// import { Container } from './style'
 
 export const Home: React.FC = () => {
   return (
