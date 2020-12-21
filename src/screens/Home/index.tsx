0f9e756d1742eb0e901986d450223f661890980f
Gui-dev <adrianon10@gmail.com>
2020-12-21 02:10:20 -0300

    feat: add helper method handleNavigationToPageDetails

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,72 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { MaterialIcons as Icon } from '@expo/vector-icons'
 
-// import { Container } from './style'
+import bannerImage from './../../assets/banner.png'
+import tennis1 from './../../assets/1.png'
+import tennis2 from './../../assets/2.png'
+import tennis3 from './../../assets/3.png'
+import tennis4 from './../../assets/4.png'
+import { ItemShoes } from './../../components/ItemShoes'
+import { Container, Header, Image, HeaderContent, Title, Separator, Genre, ButtonSearch,
+ Line, Products, Shoes } from './style'
 
 export const Home: React.FC = () => {
+
+  const { navigate } = useNavigation()
+
+  const handleNavigationToPageDetails = () => {
+    navigate('Details')
+  }
+
   return (
-    <Text>Home</Text>
+    <Container>
+      <Header>
+        <Image source={ bannerImage }/>
+
+        <HeaderContent>
+          <Title>TÊNIS</Title>
+          <Separator>-</Separator>
+          <Genre>MASCULINO</Genre>
+          <ButtonSearch onPress={ () => {} }>
+            <Icon name="filter-list" size={ 24 } color="#FFF"/>
+          </ButtonSearch>
+        </HeaderContent>
+
+      </Header>
+
+      <Line />
+
+      <Products>
+        <Title>LANÇAMENTOS</Title>
+
+        <Shoes>
+          <ItemShoes 
+            title="Nike Air Max"
+            img={ tennis1 }
+            cost="199,90"
+            onClick={ handleNavigationToPageDetails }
+          />
+          <ItemShoes 
+            title="Nike Air Max"
+            img={ tennis2 }
+            cost="599,90"
+            onClick={ handleNavigationToPageDetails }
+          />
+          <ItemShoes 
+            title="Nike Air Max"
+            img={ tennis3 }
+            cost="399,90"
+            onClick={ handleNavigationToPageDetails }
+          />
+          <ItemShoes 
+            title="Nike Air Max"
+            img={ tennis4 }
+            cost="499,90"
+            onClick={ handleNavigationToPageDetails }
+          />
+        </Shoes>
+      </Products>
+    </Container>
   )
 }
