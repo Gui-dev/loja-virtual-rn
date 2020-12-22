bf903a427b57f0daf75829593d32ab4728c01572
Gui-dev <adrianon10@gmail.com>
2020-12-22 02:36:13 -0300

    style: add style DotContainer

---
+++ src/screens/Details/style.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  width: 100%;
+  background-color: #222;
+`
+
+export const Image = styled.Image`
+  width: 100%;
+  height: 250px;
+`
+
+
+export const ContainerProduct = styled.View`
+  margin: 5%;
+`
+
+export const ContainerPrice = styled.View`
+  
+`
+
+export const Price = styled.Text`
+  font-size: 24px;
+  font-family: Anton_400Regular;
+  color: #FFF; 
+  padding: 0 2%;
+`
+
+export const ContainerTitle = styled.View`
+  opacity: 0.7;
+`
+
+export const Title = styled.Text`
+  font-size: 30px;
+  font-family: Anton_400Regular;
+  color: #FFF;
+  padding: 0 2%; 
+`
+
+export const DotContainer = styled.View`
+  flex-direction: row;
+  margin-top: 7%;
+`
