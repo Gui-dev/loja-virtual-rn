8c85619b41189d501a269d71632eeb31a671b92b
Gui-dev <adrianon10@gmail.com>
2020-12-22 02:35:40 -0300

    feat: add component Dot

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
-// import { Container } from './styles'
+import tennis1 from './../../assets/detail.png'
+import { Dot } from '../../components/Dot'
+import { Container, Image, ContainerProduct, 
+  ContainerPrice, Price, ContainerTitle, Title, DotContainer } from './style'
 
 export const Details: React.FC = () => {
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Detalhes'
+    })
+  }, [navigation])
+
   return (
-    <Text>Details</Text>
+    <Container>
+      <Image source={ tennis1 } resizeMode="cover"/>
+
+      <ContainerProduct>
+        <ContainerPrice>
+          <Price>R$ 280,90</Price>
+        </ContainerPrice>
+        <ContainerTitle>
+          <Title>Nike Air Max</Title>
+        </ContainerTitle>
+
+        <DotContainer>
+          <Dot color="#2379F4"/>
+          <Dot color="#FB6E53"/>
+          <Dot color="#DDD"/>
+          <Dot color="#000"/>
+        </DotContainer>
+      </ContainerProduct>
+    </Container>
   )
 }
